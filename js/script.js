01b2605cba2663b80a3d6a677dfd049ee25332fd
Lukas-Dahlberg-nti-johanneberg <Lukas-Dahlberg-nti-johanneberg@users.noreply.github.com>
2020-03-18 00:26:26 +0100

    Filer

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.button').addEventListener('click', function () {
+   
+    document.querySelector('.button').classList.toggle('change');
+   /* document.querySelector('.popup').classList.toggle('active');*/
+
+    document.querySelector('.popup').classList.toggle('show');
+});
+
+
